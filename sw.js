65b0909847bf597778f4f28026fdf9b6852f6d1e
Toxic2421 <razielmedina2008@gmail.com>
2026-04-29 17:36:51 -0300

    se convirtio en pwa

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'antigravity-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './imagen.png.png',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&display=swap',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
